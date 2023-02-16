523e60f8ec491d8b15166b40374cbc9ed16fc958
Prem Kshatri <101463717+premmkshatri@users.noreply.github.com>
2023-02-16 18:08:12 +0000

    Commit request package

--- amazon/amazon/spiders/search.py
+++ amazon/amazon/spiders/search.py
@@ -1,10 +1,11 @@
 import scrapy
 from scrapy_selenium import SeleniumRequest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class SearchSpider(scrapy.Spider):
     name = "search"
-    # allowed_domains = ["www.amazon.in"]
-    # start_urls = ["http://www.amazon.in/"]
 
     def start_requests(self):
         yield SeleniumRequest(
